9b757887b8a8715988d00587a3751f00a06ece12
Carson Zhang <carsonztl.zhang@mail.utoronto.ca>
2021-10-14 11:36:25 -0400

    EGuoabogeaaeg

---
+++ src/GameManager.java
@@ -0,0 +1,25 @@
+public class GameManager {
+    public void RunGame() {
+        /* Starts the game */
+    }
+
+    public void StartBattle (Battle battle) {
+        /* uses Battle class to begin a battle after the end of the choice selection*/
+    }
+    public void PromptEvents (Controller controller) {
+        /* uses Controller to ask the user to select between available game choices*/
+    }
+
+    public void updatePlayer (Gamestate gamestate, int updated_variables) {
+        /* updates player's variables using GameState*/
+    }
+
+    public void updateProvince (Gamestate gamestate, Province province, int updated_variables) {
+        /* updates a province's variables using GameState*/
+    }
+
+    public void GameStateSaver (GameState gamestate) {
+        /* uses GameState to save game*/
+    }
+
+}
