90e2fc66f05d191fedb0c3d2cc042fe61fcfbd79
Ansh757 <ansh.malhotra@mail.utoronto.ca>
2021-11-15 21:45:08 -0500

    A todo in the GameSate

--- src/com/company/gameState/gameStateElement.java
+++ src/com/company/gameState/gameStateElement.java
@@ -3,6 +3,8 @@ package com.company.gameState;
 
 import java.io.Serializable;
 
+// TODO: Can safely ignore this: For Phase 2 extendability and
+//  other things that can be saved using this
 public class gameStateElement implements Serializable{
 
     public gameStateElement(Object gameStateData, String gameStateType) {
