a3eef1b85099cd03bcda6c278e46e3576002048a
Girish Sujethan <69744717+Girish-sujethan@users.noreply.github.com>
2021-11-15 22:01:33 -0500

    Update and rename gameStateElement.java to GameStateElement.java

---
+++ src/com/company/gameState/GameStateElement.java
@@ -0,0 +1,14 @@
+
+package com.company.GameState;
+
+import java.io.Serializable;
+
+// TODO: Can safely ignore this: For Phase 2 extendability and
+//  other things that can be saved using this
+public class GameStateElement implements Serializable{
+
+    public GameStateElement(Object gameStateData, String gameStateType) {
+    }
+
+}
+
